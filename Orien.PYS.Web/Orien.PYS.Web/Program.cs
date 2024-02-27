f62440b95d0c4eebc72e826d626d456ae3f9876f
kaushalshah750 <kaushalshah750@gmail.com>
2024-02-28 01:31:16 +0530

    pipeline issue fixed and added serilog back again

--- Orien.PYS.Web/Orien.PYS.Web/Program.cs
+++ Orien.PYS.Web/Orien.PYS.Web/Program.cs
@@ -3,8 +3,14 @@ using Orien.PYS.Business.Microsoft.Extensions.DependencyInjection;
 using Orien.PYS.Data;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(config).CreateLogger();
+builder.Logging.AddSerilog(Log.Logger);
 
 builder.Services.AddAuthentication(options =>
     {
